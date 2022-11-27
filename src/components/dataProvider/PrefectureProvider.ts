9cd4eb58b56e255ddb54d62da968142ababf2db5
haminem <ryuu.shushu@gmail.com>
2022-11-27 12:03:20 +0900

    Add 都道府県のデータプロバイダを作成

---
+++ src/components/dataProvider/PrefectureProvider.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Prefecture } from "@/types/Prefecture";
+import { Provider } from "@/types/utils/Provider";
+
+export type PrefectureProvider = {
+  prefectures: Prefecture[];
+} & Provider;
+
+function PrefectureProvider(): PrefectureProvider {
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      headers: {
+        "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPrefectures(data.result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return { prefectures, isLoading, error };
+}
+
+export default PrefectureProvider;
