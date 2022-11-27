7da83d867d7a67aa662b6cc3218d97a7d4c3346d
haminem <ryuu.shushu@gmail.com>
2022-11-27 12:04:58 +0900

    Update 都道府県リストの作成とスタイルの調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PrefectureProvider from "./components/dataProvider/PrefectureProvider";
+import List from "./components/dataDisplay/List";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <List title="都道府県" dataProvider={PrefectureProvider()} />
+    </>
+  );
 }
 
 export default App;
